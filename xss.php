b71a2313d9a56b925eb20e67b377b251b5d43cef
Uththara1 <88760075+Uththara1@users.noreply.github.com>
2023-05-30 16:03:59 +0530

    Add files via upload

---
+++ xss.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Display HTML Values</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <input type="text" name="html_input" placeholder="Enter HTML value" />
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $htmlValue = $_POST['html_input'];
+        echo $htmlValue;
+    }
+    ?>
+</body>
+</html>
+
