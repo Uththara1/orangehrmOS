c680fce481bc4d1ab1a08f7c011079a7c752f8b1
Uththara1 <88760075+Uththara1@users.noreply.github.com>
2023-05-31 12:36:03 +0530

    Add files via upload

---
+++ Autocomple.php
@@ -0,0 +1,8 @@
+ <?php if (!is_null($appraisal->getHeader())) { ?>
+                            <div class="abc-form-body not-padded">
+                                <div class=" header"
+                                     id="appraisalHeader"><?php echo html_entity_decode($appraisal->getHeader()) ?></div>
+                            </div>
+                            <div class="abc-form-title">
+                            </div>
+                        <?php } ?>
